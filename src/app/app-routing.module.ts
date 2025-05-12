a05a37382fbc48af014c492fdb5e18cf53e1fc96
Rajib <saidulislamrajib1997@gmail.com>
2025-05-12 10:20:28 +0600

    Remove unnecessary things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,75 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectsComponent } from './pages/projects/projects.component';
 import { AboutComponent } from './pages/about/about.component';
-import { TopicsComponent } from './pages/topics/topics.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { RecentPostComponent } from './pages/recent-post/recent-post.component';
-import { PostComponent } from './pages/post/post.component';
 import { NotFoundComponent } from './common/not-found/not-found.component';
-import { AdminLoginComponent } from './admin/admin-login/admin-login.component';
-import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
-const routes: Routes = [
-  {
-    path: 'admin/login',
-    component: AdminLoginComponent,
-    data:{
-      title: 'admin/login',
-      reuse: true,
-      pagetype: 'view'
-    }
-  },
-  {
-    path: 'admin/dashboard',
-    component: AdminDashboardComponent,
-    data:{
-      title: 'admin/dashboard',
-      reuse: true,
-      pagetype: 'view'
-    }
-  },
+const routes: Routes = [  
   {
     path: 'home',
-    component: DashboardComponent,
+    component: AboutComponent,
     data: {
       title: 'Dashboard',
-      reuse: true,
-      pageType: 'view',
-    },
-  },
-  {
-    path: 'home/posts/:id',
-    component: PostComponent,
-    data: {
-      title: 'Dashboard',
-      reuse: true,
-      pageType: 'view',
-    },
-  },
-  {
-    path: 'topics',
-    component: TopicsComponent,
-    data: {
-      title: 'Topics',
-      reuse: true,
-      pageType: 'view',
-    },
-  },
-  {
-    path: 'recent',
-    component: RecentPostComponent,
-    data: {
-      title: 'Recent-Post',
-      reuse: true,
-      pageType: 'view',
-    },
-  },
-  {
-    path: 'projects',
-    component: ProjectsComponent,
-    data: {
-      title: 'Projects',
       reuse: true,
       pageType: 'view',
     },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,43 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
 import { PagesComponent } from './pages/pages.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
 import { AboutComponent } from './pages/about/about.component';
-import { TopicsComponent } from './pages/topics/topics.component';
-import { PopularTagsComponent } from './pages/popular-tags/popular-tags.component';
 import { FooterComponent } from './pages/footer/footer.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AdminLoginComponent } from './admin/admin-login/admin-login.component';
-import { RecentPostComponent } from './pages/recent-post/recent-post.component';
-import { PostComponent } from './pages/post/post.component';
 import { FeedbackComponent } from './common/feedback/feedback.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './common/not-found/not-found.component';
-import { CommentSectionComponent } from './pages/sharedComponent/comment-section/comment-section.component';
 import { CeilPipe } from './core/pipes/ceil.pipe';
-import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesComponent,
-    ProjectsComponent,
     AboutComponent,
-    TopicsComponent,
-    RecentPostComponent,
-    PopularTagsComponent,
     FooterComponent,
-    DashboardComponent,
-    AdminLoginComponent,
-    PostComponent,
     FeedbackComponent,
     NotFoundComponent,
-    CommentSectionComponent,
     CeilPipe,
-    AdminDashboardComponent,
-
   ],
   imports: [
     BrowserModule,
